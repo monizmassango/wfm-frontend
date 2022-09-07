124ee080381fcc8288ec077dfa802410f879457b
moniz Massango <moniz.massango@gmail.com>
2022-09-07 11:01:33 +0200

    change navbar to stay fixed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   scroll(el: string) {
     this.router.navigate(['/']).finally(() => {
       document.getElementById(el)?.scrollIntoView({
-        behavior: "smooth",
-      })
-    })
+        behavior: 'smooth',
+      });
+    });
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (number > 0) {
+      document.querySelector('nav')?.classList.add('moveTop');
+    } else {
+      document.querySelector('nav')?.classList.remove('moveTop');
+    }
+  }
 }
